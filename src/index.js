3af2a81bd9108cd57c9fbffa445fc925b4645db1
ryosuga <118461704+ryosuga@users.noreply.github.com>
2023-04-15 22:25:24 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+console.log("TEST");
+
+document.getElementById("app").innerHTML = `
+<h1>ja Vanilla!</h1>
+<p>テキストのテスト</p>
+<div>AAA
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
